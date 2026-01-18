bec19f96ff3a9df2bbae579d956421f0d3be2f12
Alugbe Idris <mrmoh00700@gmail.com>
2026-01-18 17:23:46 +0100

    prop Drilling

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,32 @@
-import ProfilePage from './ProfilePage';
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+import ProfilePage from './components/ProfilePage';
 import UserContext from './UserContext';
 
-function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
-  return (
-    <UserContext.Provider value={userData}>
-      <ProfilePage />
-    </UserContext.Provider>
-  );
+function App() {
+    return (
+        <div>
+            <WelcomeMessage />
+             <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile 
+        name="Alice" 
+        age={25} 
+        bio="Loves hiking and photography" 
+      />
+
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+
+        </div>
+
+    );
 }
 
 export default App;
