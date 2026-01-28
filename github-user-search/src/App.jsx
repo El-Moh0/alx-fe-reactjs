1650cca8a04ff51ea7de1d2a4f383bb86a976ed0
Alugbe Idris <mrmoh00700@gmail.com>
2026-01-28 21:37:49 +0100

    creatd a gitHub user search

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import Search from "./components/Search";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,11 @@
+// src/components/Search.jsx
+const Search = () => {
+  return (
+    <div>
+      <h2>GitHub User Search</h2>
+      <input type="text" placeholder="Search GitHub users..." />
+    </div>
+  );
+};
+
+export default Search;
