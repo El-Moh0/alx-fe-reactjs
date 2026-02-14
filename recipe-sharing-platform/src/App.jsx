c940224d491c276fe374de742e3b63bc235d1ddd
Alugbe Idris <mrmoh00700@gmail.com>
2026-02-14 22:01:20 +0100

    made some changes on index.html

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Recipe Sharing Platform 🍲
+    <div className="min-h-screen bg-gray-200 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind is Working 🎉
       </h1>
     </div>
   );
