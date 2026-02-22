5d7dd402f7b28981e09a5f596428e476e3fcfd68
Alugbe Idris <mrmoh00700@gmail.com>
2026-02-22 18:58:23 +0100

    update the app file

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,39 +1,20 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Home.jsx';
-import Profile from './components/Profile.jsx';
-import ProfileDetails from './components/ProfileDetails.jsx';
-import ProfileSettings from './components/ProfileSettings.jsx';
-import Post from './components/Post.jsx';
-import Login from './components/Login.jsx';
-import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Profile from './components/Profile.jsx'
+import ProfileDetails from './components/ProfileDetails.jsx'
+import ProfileSettings from './components/ProfileSettings.jsx'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-
-      {/* Protected profile routes */}
-      <Route
-        path="/profile/*"
-        element={
-          <ProtectedRoute>
-            <Profile />
-          </ProtectedRoute>
-        }
-      >
-        {/* Nested routes under Profile */}
-        <Route path="details" element={<ProfileDetails />} />
-        <Route path="settings" element={<ProfileSettings />} />
-      </Route>
-
-      {/* Dynamic route for blog post */}
-      <Route path="/post/:postId" element={<Post />} />
-
-      {/* Login route */}
-      <Route path="/login" element={<Login />} />
-
-      {/* Catch-all redirect */}
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />}>
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
