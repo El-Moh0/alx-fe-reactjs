edce895cdb5448bb6974c9a7982c9ef90ba1c88f
Alugbe Idris <mrmoh00700@gmail.com>
2026-02-22 21:33:30 +0100

    created a blogpost file

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,41 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Profile from './components/Profile.jsx'
-import ProfileDetails from './components/ProfileDetails.jsx'
-import ProfileSettings from './components/ProfileSettings.jsx'
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+// Pages / Components
+import Home from './components/Home.jsx';
+import Profile from './components/Profile.jsx';
+import ProfileDetails from './components/ProfileDetails.jsx';
+import ProfileSettings from './components/ProfileSettings.jsx';
+import BlogPost from './components/BlogPost.jsx';
+import Login from './components/Login.jsx';
+import ProtectedRoute from './components/ProtectedRoute.jsx';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/profile/*" element={<Profile />}>
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <Routes>
+      {/* Home page */}
+      <Route path="/" element={<Home />} />
+
+      {/* Protected Profile route with nested routes */}
+      <Route
+        path="/profile/*"
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      >
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Route>
+
+      {/* Dynamic Blog Post route */}
+      <Route path="/blog/:id" element={<BlogPost />} />
+
+      {/* Login page */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Catch-all redirect */}
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
