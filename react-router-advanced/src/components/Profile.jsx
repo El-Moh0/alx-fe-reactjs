4bc9f056fb07184296e92b4dcc49a6c8f171fc75
Alugbe Idris <mrmoh00700@gmail.com>
2026-02-22 21:29:38 +0100

    defined my nested routes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,16 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import ProfileDetails from './ProfileDetails.jsx';
+import ProfileSettings from './ProfileSettings.jsx';
+
 import { Link, Outlet } from 'react-router-dom';
 
 export default function Profile() {
   return (
     <div>
       <h1>Profile Page</h1>
-
       <nav>
         <Link to="details">Details</Link> |{' '}
         <Link to="settings">Settings</Link>
       </nav>
-
-      {/* Nested routes will render here */}
       <Outlet />
     </div>
   );
